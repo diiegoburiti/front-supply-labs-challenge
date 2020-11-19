9758dbf63ccb7a4c6d885dbbfabfd27cb9e21b23
Diego Buriti <bs.diego97@gmail.com>
2020-11-19 17:16:02 -0300

    add table component

---
+++ src/components/table/table.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import useOrdenation from '../../hooks/useOrdenation'
+import styles from './styles/table.module.css'
+
+export default function Table(props) {
+  const { items, requestSort, sortConfig } = useOrdenation(props.data)
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined
+  }
+  /*   const renderBody = (items) => {
+    return items.map((user) => (
+      <tr key={user.id}>
+        <td className={styles.id}>{user.id}</td>
+        <td>{user.name}</td>
+        <td>{user.username}</td>
+        <td>{user.email}</td>
+        <td className={styles.age}>{user.age}</td>
+        <td>
+          <Link to={`/user/${user.id}`}>
+            <button className={styles.Link}>+</button>
+          </Link>
+        </td>
+      </tr>
+    ))
+  } */
+
+  return (
+    <table className={styles.table}>
+      <thead className={styles.tableHeader}>
+        <tr>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('id')}
+              className={`btnOrdednate ${getClassNamesFor('id')} `}
+              title="Ordenar por ID"
+            >
+              id
+            </button>
+          </th>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>email</th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('age')}
+              className={`btnOrdednate ${getClassNamesFor('age')} `}
+              title="Ordenar por Idade"
+            >
+              idade
+            </button>
+          </th>
+
+          <th>acoes</th>
+        </tr>
+      </thead>
+      <tbody className={styles.tableBody}>
+        {items.map((user) => (
+          <tr key={user.id}>
+            <td className={styles.id}>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td className={styles.age}>{user.age}</td>
+            <td>
+              <Link to={`/user/${user.id}`}>
+                <button className={styles.Link}>+</button>
+              </Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
